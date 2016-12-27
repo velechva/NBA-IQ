8194bf87247d69ec9bf8d7bb3a06c55f45aa325f
Victor Velechovsky <vic.vele@gmail.com>
2016-12-27 06:15:17 -0800

    Started working on data scrapping

---
+++ src/Constants.py
@@ -0,0 +1,55 @@
+# Season: NNNN-NN (eg. 1995-96)
+
+teams = {
+    "Atlanta Hawks": 1610612737,
+    "Boston Celtics": 1610612738,
+    "Brooklyn Nets": 1610612751,
+    "Charlotte Hornets": 1610612766,
+    "Chicago Bulls": 1610612741,
+    "Cleveland Cavaliers": 1610612739,
+    "Dallas Mavericks": 1610612742,
+    "Denver Nuggets": 1610612743,
+    "Detroit Pistons": 1610612765,
+    "Golden State Warriors": 1610612744,
+    "Houston Rockets": 1610612745,
+    "Indiana Pacers": 1610612754,
+    "Los Angeles Clippers": 1610612746,
+    "Los Angeles Lakers": 1610612747,
+    "Memphis Grizzlies": 1610612763,
+    "Miami Heat": 1610612748,
+    "Milwaukee Bucks": 1610612749,
+    "Minnesota Timberwolves": 1610612750,
+    "New Orleans Pelicans": 1610612740,
+    "New York Knicks": 1610612752,
+    "Oklahoma City Thunder": 1610612760,
+    "Orlando Magic": 1610612753,
+    "Philadelphia 76ers": 1610612755,
+    "Phoenix Suns": 1610612756,
+    "Portland Trail Blazers": 1610612757,
+    "Sacramento Kings": 1610612758,
+    "San Antonio Spurs": 1610612759,
+    "Toronto Raptors": 1610612761,
+    "Utah Jazz": 1610612762,
+    "Washington Wizards": 1610612764
+}
+
+seasonType = {
+    "Regular Season": "Regular+Season",
+    "Pre Season": "Pre+Season",
+    "Playoffs": "Playoffs",
+    "All-Star": "All-Star",
+    "All Star": "All+Star",
+    "Preseason": "Preseason"
+}
+
+years = [
+    "2010-11",
+    "2011-12",
+    "2012-13",
+    "2013-14",
+    "2014-15",
+    "2015-16",
+    "2016-17"
+]
+
+leagueID = 00
